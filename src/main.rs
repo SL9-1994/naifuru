003fb6ed50ed6df0e0f67f02add27600d2b6c88e
yurari <320sl91994@gmail.com>
2025-05-03 23:44:55 +0900

    feat:✨ エラーハンドリングを強化し、CLI引数のバリデーションとロギングを統合

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use log::{debug, error};
+use naifuru::{bail_on_error, cli::Args, error::AppError, logging::init_logger};
+
+const DEFAULT_ERROR_EXIT_CODE: i32 = 1;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(errors) = run() {
+        for error in &errors {
+            error!("{}", error);
+        }
+
+        // 最初のエラーからexit_codeを決定、また、exit_codeを取得できない場合はDEFAULT_ERROR_EXIT_CODEで終了します。
+        let exit_code = errors
+            .first()
+            .map_or(DEFAULT_ERROR_EXIT_CODE, |e| e.exit_code());
+
+        bail_on_error!(exit_code);
+    }
+}
+
+fn run() -> Result<(), Vec<AppError>> {
+    let args = Args::new();
+
+    init_logger(args.log_level.into()).unwrap();
+    debug!("The logging level has been set successfully.");
+
+    args.validate()?;
+    debug!("The CLI args have been validated successfully.");
+
+    Ok(())
 }
