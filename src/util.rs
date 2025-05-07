e45b6a3fab1ea63cad9611b6921d7167ccad42b0
yurari <320sl91994@gmail.com>
2025-05-07 23:01:34 +0900

    feat:✨ リトルエンディアンおよびビッグエンディアンに変換する関数を追加

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
 use std::{fs::File, io::Read, path::Path};
 
+use byteorder::{ByteOrder, LittleEndian};
+
 use crate::analysis_config::TextOrBinary;
 
 pub fn read_text(path: &Path) -> Result<String, std::io::Error> {
@@ -27,6 +29,39 @@ pub fn to_fixed_chunks<const N: usize>(data: Vec<u8>) -> Vec<[u8; N]> {
         .map(|chunk| {
             let arr: [u8; N] = chunk.try_into().expect("Chunk size mismatch");
             arr
+        })
+        .collect()
+}
+
+// 特段の理由がない限り，基本的にはLittie Endianに変換して処理
+pub fn to_little_endian<const N: usize>(input: Vec<[u8; N]>) -> Vec<[u8; N]> {
+    input
+        .into_iter()
+        .map(|chunk| {
+            // Nバイトをリトルエンディアンとして解釈
+            let num = LittleEndian::read_i32(&chunk);
+
+            // 数値をリトルエンディアンで再度バイト列に変換
+            let mut result = [0u8; N];
+            LittleEndian::write_i32(&mut result, num);
+
+            result
+        })
+        .collect()
+}
+
+pub fn to_big_endian<const N: usize>(input: Vec<[u8; N]>) -> Vec<[u8; N]> {
+    input
+        .into_iter()
+        .map(|chunk| {
+            // Nバイトをビッグエンディアンとして解釈
+            let num = LittleEndian::read_i32(&chunk);
+
+            // 数値をビッグエンディアンで再度バイト列に変換
+            let mut result = [0u8; N];
+            LittleEndian::write_i32(&mut result, num);
+
+            result
         })
         .collect()
 }
