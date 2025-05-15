ec4159f96c2e26122213225499b0e1ef4470173f
yurari <320sl91994@gmail.com>
2025-05-15 20:34:58 +0900

    ✨ feat: A/D値とスケールファクタを用いて加速度値を計算する関数を追加

--- src/extractor/jp_nied_knet_une.rs
+++ src/extractor/jp_nied_knet_une.rs
@@ -41,7 +41,14 @@ impl JpNiedKnetUneExtractor {
     }
 }
 
+// A/D値とスケールファクタとの積を計算し，加速度値を求める
+/// K-net, Kik-net, ASCII type only.
+fn _to_acceleration_using_scale_factor(scale_factor: f64, ad_values: Vec<f64>) -> Vec<f64> {
+    let accs = ad_values.iter().map(|acc| acc * scale_factor).collect();
+    accs
+}
+
 // 計算量削減のため，先にscale factorの分数を計算する
-fn _calculate_scale_factor(numerator: u64, denominator: u64) -> f64 {
+fn calculate_scale_factor(numerator: u64, denominator: u64) -> f64 {
     (numerator / denominator) as f64
 }
